91c787f2bebe6fc15cfe44e2c7cf9fb3af70b9d2
Lourens Veen <l.veen@esciencecenter.nl>
2024-01-18 16:46:18 +0100

    Print a helpful message if people try to install the old way

---
+++ setup.py
@@ -0,0 +1,8 @@
+msg = '\nAMUSE has a new build system! See the documentation on how to install it.\n'
+msg += 'Or, if you\'re feeling brave and have GNU Make installed, type\n\n'
+msg += '    make configure\n\n'
+msg += 'and follow the instructions.\n\n'
+
+print(msg)
+quit(1)
+
